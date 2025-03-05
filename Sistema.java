a4cef4acc1029310f16cdf31c9d7a27e029a8571
gabriellvz <gabrielalvescruz0@gmail.com>
2025-03-05 15:13:26 -0300

    Adicionando metodos de crud

--- Sistema.java
+++ Sistema.java
@@ -7,7 +7,55 @@ public class Sistema {
         this.clientes = new ArrayList<>();
     }
 
+    
     public void adicionar(String nome, String sobrenome, String senha, String email) {
-        this.clientes.add(new Cliente(String nome, String sobrenome, String senha, String email));
+    	Cliente cliente = new Cliente(nome, sobrenome, senha, email);
+        this.clientes.add(cliente);
         System.out.println("Cliente adicionado: " + cliente.getNome() + " " + cliente.getSobrenome());
     }
+    
+    //Listando
+    public void listar() {
+    	if (clientes.isEmpty()) {
+    		System.out.println("Nenhum cliente foi encontrado.");
+    	}
+    	else {
+	    	for (Cliente cliente : clientes) {
+	   			System.out.println(cliente);
+	   		}
+    	}
+  
+    }
+    
+    //Atualiza dados pesquisando por email
+    public void atualizar(String emailBusca, String nome, String sobrenome, String senha, String email) {
+    	boolean encontrado = false;
+    	
+    	for (Cliente cliente : clientes) {
+    		if (cliente.getEmail().equals(emailBusca)) {
+    			cliente.setNome(nome);
+    			cliente.setSobrenome(sobrenome);
+    			cliente.setSenha(senha);
+    			cliente.setEmail(email);
+    			System.out.println("Cliente atualizado: " + cliente);
+    			encontrado = true; 
+    			break; 
+    		}
+    	}
+    	if(!encontrado) {    		
+    		System.out.println("Email do cliente não encontrado: " + emailBusca);
+    	}
+    }
+    
+    //Remover pesquisando o nome
+    public void remover(String nome) {
+    	if(clientes.removeIf(cliente -> cliente.getNome().equals(nome))) {
+    		System.out.println("Cliente: " + nome + " removido");
+    	}
+    	else {
+    		System.out.println("Cliente: " + nome + " não encontrado.");
+    	}
+    }
+}
+    
+    
